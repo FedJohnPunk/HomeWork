0bbfb4974bff6bf214539d8a4b3f35eda321f1fe
FedJohn <ivan.fedyun@gmail.com>
2022-12-17 20:59:04 +0300

    Сделано первое задание урока

---
+++ Lesson4.1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // Программа генерации случайной матрицы
+        Console.WriteLine("Введите количество строк в матрице:");
+        int matrixRow = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите количество столбцов в матрице:");
+        int matrixCol = int.Parse(Console.ReadLine());
+
+        int[,] matrix = new int[matrixRow, matrixCol];
+        Random r = new Random();
+        int sum = 0;
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i, j] = r.Next(10);
+                sum += matrix[i, j];
+                Console.Write($"{matrix[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine($"Сумма всех элементов матрицы: {sum}");
+        Console.ReadLine();
+
+    }
+}
