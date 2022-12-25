29e2dcd9e7daa9f2eb8afbcb88c507e02cbecbb5
FedJohn <ivan.fedyun@gmail.com>
2022-12-25 14:58:15 +0300

    Задание 5.1

---
+++ Lesson5.1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Lesson5._1;
+
+internal class Program
+{
+
+    /// <summary>
+    /// Метод, который переводит строку в массив слов.
+    /// <param name="Text"></param>
+    /// <returns></returns>
+    static string[] SplitText(string text)
+    {
+        string[] strings = text.Split();
+        return strings;
+    }
+
+    /// <summary>
+    /// Метод, который выводит каждое слово в отдельной строке.
+    /// </summary>
+    /// <returns></returns>
+    static void Print(string[] strings)
+    {
+        Console.WriteLine();
+        foreach (string i in strings)
+        {
+            Console.WriteLine(i);
+        }
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите предложение:");
+        string text = Console.ReadLine();
+        string[] st = SplitText(text);
+        Print(st);
+    }
+}
