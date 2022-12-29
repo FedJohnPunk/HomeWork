29be29379c606f123be0d0c43e0bdfe0b7eff8b1
FedJohn <ivan.fedyun@gmail.com>
2022-12-29 19:27:36 +0300

    Сделал задание 6

--- Lesson6/Program.cs
+++ Lesson6/Program.cs
@@ -5,12 +5,30 @@ internal class Progsram
     {
         bool existCheck = File.Exists(@"C:\UserData\Сотрудники.txt");
         string[] infoNames = { "Ф.И.О.", "Возраст", "Рост", "Дата рождения", "Место рождения" };
-        //FileWriter(existCheck, infoNames);
-        FileReader(infoNames);
+
+        WriteOrRead(existCheck, infoNames);
     }
+    static void WriteOrRead(bool existCheck, string[] info)
+    {
+        Console.WriteLine("Для чтения нажмите 1, для записи нажмите 2:");
+        char key = Console.ReadKey(true).KeyChar;
+        if (char.ToLower(key) == '1')
+        {
+            FileReader(info);
+        }
+        else if (char.ToLower(key) == '2')
+        {
+            FileWriter(existCheck, info);
+        }
+        else
+        {
+            Console.WriteLine("Некоректный ввод.");
+        }
+    }
+
     static void FileWriter(bool fileExCheck, string[] info)
     {
-        using (StreamWriter sw = new StreamWriter(@"C:\UserData\Сотрудники.txt", fileExCheck))
+        using (StreamWriter sw = new StreamWriter(@"C:\UserData\Сотрудники.txt5", fileExCheck))
         {
             char key = '1';
 
@@ -31,7 +49,7 @@ internal class Progsram
                 }
 
                 sw.WriteLine(userData);
-                Console.Write("Для продолжения введите '1', для завершения нажмите любую клавишу:");
+                Console.WriteLine("Для новой записи нажмите один '1', для завершения нажмите любую клавишу:");
                 key = Console.ReadKey(true).KeyChar;
             } while (char.ToLower(key) == '1');
         }
@@ -47,15 +65,20 @@ internal class Progsram
             foreach (var line in data)
             {
                 string[] splitedData = line.Split('#');
+                string userData = string.Empty;
 
-                for (int i = 0; i < splitedData.Length; i++)
+                userData += $"ID: {splitedData[0]}, ";
+                userData += $"Дата записи: {splitedData[1]}, ";
+                int j = 0;
+                for (int i = 2; i < splitedData.Length; i++)
                 {
-                    string userData = string.Empty;
-                    userData += $"{info[i]}: {splitedData[i]}";
-                    Console.WriteLine(userData);
-                    Console.ReadKey();
+                    userData += $"{info[j]}: {splitedData[i]}, ";
+                    j++;
                 }
+                Console.WriteLine(userData);
+
             }
+            Console.ReadLine();
         }
     }
 }
